9f001814098c9b35a8fddc751484f51b48950a1c
Ermano Arruda <Ermano.Arruda@dyson.com>
2021-05-29 00:02:25 +0100

    high tech

--- mock_script.sh
+++ mock_script.sh
@@ -2,18 +2,22 @@
 
 list=("bunny" "yellow" "bla" "doge")
 
-# selecting the list of scales and creating a temporary list
+# selecting the list of scales and creating a temporary list for it
 awk '{ print $4 }' sums.txt > temp_test.txt
 list_scales=($(cat temp_test.txt))
 
+# getting the size of our list
 list_size=${#list[@]}
 
+# iterating over our two lists and getting corresponding pairs
 for (( idx=0;idx<${list_size}; idx++ ));
 do
 
+# Getting the pairs
 i=${list[$idx]}
 tmp_scale=${list_scales[$idx]}
 
+# Printing the pairs
 echo "name: $i, scale: $tmp_scale"
 
 done
